59980a697a7708753d3183b082f4ad6e7c1ebae5
Thibault mathian <thibault.mathiant@free.fr>
2022-08-09 16:26:41 +0200

    test changes UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,9 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js, A new Thibault project</a>
-        </h1>
+        <h1 className={styles.title}>Welcome to a new Thibault project</h1>
 
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.tsx</code>

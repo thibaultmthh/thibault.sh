2e30ef7e75073e07cd3bf2238b983514b2625abf
Thibault mathian <thibault.mathiant@free.fr>
2022-08-10 11:48:27 +0200

    sass + tailwind

---
+++ pages/testtw.tsx
@@ -0,0 +1,7 @@
+export default function Testtw() {
+  return (
+    <div className="bg-midnight">
+      <h1 className="text-3xl font-bold underline">Welcome to thibault project testing</h1>
+    </div>
+  );
+}

9ba01c83f3a69db492c88dca4d7d372ad74a4fed
Thibault MATHIAN <dev@thibault.sh>
2022-08-17 11:44:42 +0200

    change pdp

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,13 @@ export default function FourOhFour() {
   return (
     <div className="grid place-content-center h-screen">
       <div className="flex flex-col">
-        <Image src="https://media.giphy.com/media/Bp3dFfoqpCKFyXuSzP/giphy.gif" height={200} width={200} alt="lost" />
+        <Image
+          src="https://media.giphy.com/media/Bp3dFfoqpCKFyXuSzP/giphy.gif"
+          loading="lazy"
+          height={200}
+          width={200}
+          alt="404"
+        />
         <Link href="/">
           <a className="text-violet-400 font-medium underline mt-5 text-center">Back to a safe place</a>
         </Link>

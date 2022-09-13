997f3e08639f9024b0ba15aa2032cb5619ee2c5e
Thibault Mathian <dev@thibault.sh>
2022-09-13 21:06:50 +0200

    Updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import "../styles/globals.scss";
 import "styles/Projects.scss";
-import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
-
+import { QueryClient, QueryClientProvider, QueryFunction, QueryKey, useQuery } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import axios, { Axios } from "axios";
+
+// Define a default query function that will receive the query key
+const defaultQueryFn: QueryFunction<unknown, QueryKey> = async ({ queryKey }) => {
+  const { data } = await axios.get(queryKey[0] as string);
+  return data;
+};
+
+// provide the default query function to your app with defaultOptions
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: defaultQueryFn,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/guestbook.tsx
+++ pages/guestbook.tsx
@@ -1,32 +1,23 @@
+import { Prisma } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import Button from "components/Button";
+import CanvasGuestbook from "components/CanvasGuestbook";
 import Contener from "components/Contener";
 import Message from "components/Message";
 import PageTitle from "components/PageTitle";
+import Pixel from "components/Pixel";
+import StickCursor from "components/StickCursor";
+import guestbookTo2D from "lib/guestbook";
 import { useState } from "react";
 
 export default function Guestbook() {
-  const l = [
-    "red",
-    "green",
-    "red",
-    "violet",
-    "red",
-    "green",
-    "red",
-    "green",
-    "red",
-    "green",
-    "red",
-    "green",
-    "red",
-    "green",
-    "red",
-    "green",
-    "red",
-    "green",
-    "red",
-    "green",
-  ];
-  const [guestbook, setGuestbook] = useState([l, l, l, l, l, l, l, l, l, l, l, l, l, l, l, l, l, l, l, l]);
+  const { data, isLoading, isError } = useQuery(["/api/guestbook/"]) as {
+    data: Prisma.GuestbookGetPayload<{}>[];
+    isLoading: boolean;
+    isError: boolean;
+  };
+
+  const canvas = guestbookTo2D(data || []);
 
   return (
     <Contener title="Guestbook - Thibault Mathian" description="Leave a message on this guestbook">
@@ -35,29 +26,29 @@ export default function Guestbook() {
         description="Leave a pixel below, this is community art created by this website visitors. For each pixels you can leave a short message. "
       />
       <div className="mb-7">
-        <h1 className="font-bold text-2xl text-white mb-7">Canvas : </h1>
-        <div className="grid place-items-center">
+        <Button>Leave a message</Button>
+        <div className="p-2 m-2 border border-violet-600 rounded-md  ">
           <div>
-            {guestbook.map((row, rowIndex) => (
-              <div key={rowIndex} className="flex ">
-                {row.map((color, colorIndex) => (
-                  <div
-                    key={colorIndex}
-                    style={{ backgroundColor: color }}
-                    className="w-4 h-4 sm:w-6 sm:h-6  border-violet-300 hover:border  "
-                  ></div>
-                ))}
-              </div>
-            ))}
+            <label htmlFor="head" className="text-white">
+              Choose a color
+            </label>
+            <input type="color" id="head" name="head" value="#e66465" />
           </div>
+
+          <StickCursor offsetY={-30}>Choose a spot</StickCursor>
+          <input type="text" id="message" name="message" placeholder="Message" />
+          <Button>Send</Button>
         </div>
+      </div>
+      <div className="mb-7">
+        <h1 className="font-bold text-2xl text-white mb-7">Canvas : </h1>
+        <CanvasGuestbook canvas={canvas} isReady={!isLoading && !isError} />
       </div>
       <div>
         <h1 className="font-bold text-2xl text-white mb-7">Guestbook : </h1>
-        <Message />
-        <Message />
-        <Message />
-        <Message />
+        {data?.map((row, rowIndex) => (
+          <Message key={row.id + "m"} post={row} canvas={canvas} />
+        ))}
       </div>
     </Contener>
   );

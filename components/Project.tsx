98496af67fc3ed1a40014829f608efd5487ad3d7
Thibault Mathian <dev@thibault.sh>
2023-06-12 12:48:22 +0200

    x

--- components/Project.tsx
+++ components/Project.tsx
@@ -16,43 +16,43 @@ export interface IProject {
 
 export default function Project({ project }: { project: IProject }) {
   return (
-    <div className=" py-8 max-w-lg  lg:max-w-4xl ">
-      <div className="mb-3">
-        <span className="font-medium text-xl text-white mr-5 ">{project.title}</span>
-        <span className="text-neutral-300 text-sm">
-          {project.dateStarted.split("-")[0]} - {project.dateFinished.split("-")[0]}
-        </span>
-      </div>
-
-      <div className="block lg:flex ">
-        <div className="w-lg  md:max-w-xl">
-          <Image
-            src={project.image.asset.url}
-            height={project.image.asset.metadata.dimensions.height}
-            width={project.image.asset.metadata.dimensions.width}
-            alt="parakeet"
-          />
+    <Link href={`/projects/${project.slug.current}`}>
+      <div className=" py-8 max-w-lg  lg:max-w-4xl ">
+        <div className="mb-3">
+          <span className="font-medium text-xl text-white mr-5 ">{project.title}</span>
+          <span className="text-neutral-300 text-sm">
+            {project.dateStarted.split("-")[0]} - {project.dateFinished.split("-")[0]}
+          </span>
         </div>
-        <div className="mt-2 lg:ml-5">
-          <div className=" hidden lg:block ">
-            <h2 className="text-white font-medium">Description: </h2>
-            <p className="text-white">{project.shortDescription}</p>
+
+        <div className="block lg:flex ">
+          <div className="w-lg  md:max-w-xl">
+            <Image
+              src={project.image.asset.url}
+              height={project.image.asset.metadata.dimensions.height}
+              width={project.image.asset.metadata.dimensions.width}
+              alt="parakeet"
+            />
           </div>
-          <div className=" mt-2 sm:mt-0">
-            <ul>
-              {project.tags.map((tag) => (
-                <Keyword keyword={tag.tag} key={tag.tag} />
-              ))}
-            </ul>
+          <div className="mt-2 lg:ml-5">
+            <div className=" hidden lg:block ">
+              <h2 className="text-white font-medium">Description: </h2>
+              <p className="text-white">{project.shortDescription}</p>
+            </div>
+            <div className=" mt-2 sm:mt-0">
+              <ul>
+                {project.tags.map((tag) => (
+                  <Keyword keyword={tag.tag} key={tag.tag} />
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="flex justify-around mt-2 sm:block sm:mt-0 sm:float-right">
-        <Link href={`/projects/${project.slug.current}`}>
+        <div className="flex justify-around mt-2 sm:block sm:mt-0 sm:float-right">
           <Button>Learn more</Button>
-        </Link>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }

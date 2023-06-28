48cf964f819815c82dec76b980024c0552217c17
Thibault Mathian <dev@thibault.sh>
2023-06-28 11:40:37 +0200

    x

--- app/other/privacy/page.tsx
+++ app/other/privacy/page.tsx
@@ -1,39 +1,39 @@
 export default function Page() {
   return (
     <>
-      <h1 className="text-4xl font-bold mb-4">Choicyful Privacy Policy</h1>
+      <h1 className="text-4xl font-bold mb-4 text-white">Choicyful Privacy Policy</h1>
       <p className="text-lg">
         Choicyful provides applications (&quot;the Service&quot;) to merchants who use Shopify to build and maintain
         their online store. This Privacy Policy provides detailed information on how data is collected, used, and shared
         when you install or use our apps with your Shopify store.
       </p>
 
-      <h2 className="text-2xl font-bold mt-4">Personal Data We Process</h2>
-      <p className="text-lg">
+      <h2 className="text-2xl font-bold mt-4 text-white">Personal Data We Process</h2>
+      <p className="text-lg text-white">
         When you install our apps, we are automatically able to access certain types of information from your Shopify
         account. See more detailed information in Shopify’s help docs.
       </p>
-      <p className="text-lg">
+      <p className="text-lg text-white">
         In addition, we store the following personal information: Emails, names, and location of our customers that are
         necessary for localized email notifications, reports, support, product updates, and reminders.
       </p>
 
-      <h2 className="text-2xl font-bold mt-4">How We Use Personal Data</h2>
-      <p className="text-lg">
+      <h2 className="text-2xl font-bold mt-4 text-white">How We Use Personal Data</h2>
+      <p className="text-lg text-white">
         We use the personal information we collect from you in order to provide the Service and to operate our apps.
         Additionally, we use the personal information to: communicate with you, optimize or improve the Service, provide
         you with information about our products, and handle software bugs or support requests.
       </p>
 
-      <h2 className="text-2xl font-bold mt-4">Sharing Your Information</h2>
-      <p className="text-lg">
+      <h2 className="text-2xl font-bold mt-4 text-white">Sharing Your Information</h2>
+      <p className="text-lg text-white">
         We do not share your Personal Information with any third parties. We may share your Personal Information to
         comply with applicable laws and regulations, to respond to a subpoena, search warrant, or other lawful request
         for information we receive, or to otherwise protect our rights.
       </p>
 
-      <h2 className="text-2xl font-bold mt-4">Security and Data Retention</h2>
-      <p className="text-lg">
+      <h2 className="text-2xl font-bold mt-4 text-white">Security and Data Retention</h2>
+      <p className="text-lg text-white">
         We retain your Personal Information for one year or until you ask us to delete it. If you ask us to delete the
         Information, we may have to retain it to comply with other legal obligations, resolve disputes, and enforce our
         agreements. We implement measures to secure your Information and to avoid risks of damage, loss of information,
@@ -41,8 +41,8 @@ export default function Page() {
         security.
       </p>
 
-      <h2 className="text-2xl font-bold mt-4">Your Rights</h2>
-      <p className="text-lg">
+      <h2 className="text-2xl font-bold mt-4 text-white">Your Rights</h2>
+      <p className="text-lg text-white">
         If you are in the EU, you have the right to access, update or delete your Information and obtain a copy of your
         information we store on our systems and have us update, correct or delete it. You are also entitled to obtain
         your Information (excluding data obtained from other sources) in a structured, commonly used and
@@ -51,28 +51,28 @@ export default function Page() {
         <a href="mailto:contact@choicyful.com">contact@choicyful.com</a>. You have a right to submit a complaint to the
         relevant supervisory data protection authority.
       </p>
-      <p className="text-lg">
+      <p className="text-lg text-white">
         If you are in the EU, you can lodge a complaint to the supervisory authority under the General Data Protection
         Regulations (&quot;GDPR&quot;), in particular in the Member State of your residence, place of work, or where the
         alleged infringement of the GDPR occurred.
       </p>
 
-      <h2 className="text-2xl font-bold mt-4">Age Of Consent</h2>
-      <p className="text-lg">
+      <h2 className="text-2xl font-bold mt-4 text-white">Age Of Consent</h2>
+      <p className="text-lg text-white">
         By using our apps, you indicate that you are at least 18 years of age or at least the age of majority in your
         state or province of residence to use the App.
       </p>
 
-      <h2 className="text-2xl font-bold mt-4">Changes To The Privacy Policy</h2>
-      <p className="text-lg">
+      <h2 className="text-2xl font-bold mt-4 text-white">Changes To The Privacy Policy</h2>
+      <p className="text-lg text-white">
         We may update this privacy policy at any time in order to reflect, for example, changes to our practices or for
         other operational, legal, or regulatory reasons. If we make changes to this policy, we will notify you by email
         of updates, so that you are aware of information we collect, how we use it, and under what circumstances, if
         any.
       </p>
 
-      <h2 className="text-2xl font-bold mt-4">Contact Information</h2>
-      <p className="text-lg">
+      <h2 className="text-2xl font-bold mt-4 text-white">Contact Information</h2>
+      <p className="text-lg text-white">
         If you would like to access, correct, amend or delete any personal information, or require any additional
         information about our privacy policy please contact us at{" "}
         <a href="mailto:contact@choicyful.com">contact@choicyful.com</a>.
